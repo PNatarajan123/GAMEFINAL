608ec81f6dabc23f132cd6046ee7a00900afa3b1
Praveen Natarajan <dsanjay@gmail.com>
2020-03-11 09:15:27 -0700

    updated CODE

--- GAMEFINAL/ViewController.swift
+++ GAMEFINAL/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+class PythagController: UIViewController {
+    
+    // IBOutlet connects to Storyboard items, I had problems with Drag
+    @IBOutlet weak var side1: UITextField!
+    @IBOutlet weak var side2: UITextField!
+    @IBOutlet weak var side3: UILabel!
+    
+    // Loads at initialization
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Initialize Outlets
+        side1.text = "0.0"
+        side2.text = "0.0"
+        side3.text = "0.0"
     }
 
+    // IBAction created with Drag from Storyboard
+   @IBAction func calculate(sender: UIButton) {
+        // let is for values that are not modified
+        let s1 = Float(side1.text!) // Input value, !(Bang) avoids errors
+        let s2 = Float(side2.text!)
 
+        // Pythagorean Calculation
+        let s3 = sqrt( (s1! * s1!) + (s2! * s2!) )
+
+        // Set calculation to screen
+        side3.text = String(s3)
+        print("Pyth  \(side3.text!)")
+    
+    }
 }
-
